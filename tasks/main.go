24710f24a19e47af96752fbe6c2632ffd7586926
Tanvi-19 <tanvi19kulkarni@example.com>
2021-05-18 15:37:13 +0530

    Updated

--- tasks/main.go
+++ tasks/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "github.com/gophercises/tasks/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/gophercises/tasks/cmd"
+	"github.com/gophercises/tasks/db"
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
-	cmd.Execute()
+	home,_ := homedir.Dir()
+	path := filepath.Join(home,"tasks.db")
+    must(db.Init(path))
+	fmt.Println("DB CONNECT")
+	must(cmd.RootCmd.Execute())
+}
+
+func must(err error){
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
